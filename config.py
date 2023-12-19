6fc7aac5dc46bc1d365141637f2412946c719a29
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2023-12-19 13:14:11 +0500

    to create odoo.conf file

---
+++ config.py
@@ -0,0 +1,25 @@
+import configparser
+
+
+
+odoo_app_config_path = './debian/odoo.conf'
+config = configparser.ConfigParser()
+config.read(odoo_app_config_path)
+
+admin_passwd = config.get('options', 'admin_passwd')
+config = configparser.ConfigParser()
+
+
+config['options'] = {
+    'db_host': '18.130.231.81',
+    'db_port': '31000',
+    'db_user': 'odoo',
+    'db_password': 'odoo',
+    'addons_path': '/opt/odoo/addons',
+    'admin_passwd': str(admin_passwd)
+}
+
+
+with open("./config/odoo.conf", 'w') as configfile:
+    config.write(configfile)
+
