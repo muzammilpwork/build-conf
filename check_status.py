6341d9ee5d7d68b0f937ec01f4a1c498b4d268af
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2023-12-29 15:34:52 +0500

    Update check_status.py

--- check_status.py
+++ check_status.py
@@ -9,16 +9,19 @@ pod_url = os.environ['POD_URL']
 params = {"id": str(db_id), "url": str(pod_url)+":8069"}
 headers = {"Authorization": f"Bearer {auth_token}"}
 
-print("########### url ", update_url)
-print("########### parameters ", params)
-print("########### headers ", headers)
-
 response = requests.get(update_url, params=params, headers=headers)
-
-# Check the response
 if response.status_code == 200:
     print("Request was successful")
     print("Response content:", response.text)
 else:
     print("Request failed with status code:", response.status_code)
     print("Response content:", response.text)
+
+
+response_app = requests.get(str(pod_url)+":8069")
+if response_app.status_code == 200:
+    print("Request on app was successful")
+    print("Your app is running....")
+else:
+    print("Request failed with status code:", response_app.status_code)
+    print("Response content:", response_app.text)
