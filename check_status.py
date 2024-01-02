559506fcfd1586e13fe3dd3067dfc72488d4798c
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2024-01-02 14:56:45 +0500

    Update check_status.py

--- check_status.py
+++ check_status.py
@@ -18,10 +18,10 @@ else:
     print("Response content:", response.text)
 
 
-response_app = requests.get(str(pod_url)+":8069")
-if response_app.status_code == 200:
-    print("Request on app was successful")
-    print("Your app is running....")
-else:
-    print("Request failed with status code:", response_app.status_code)
-    print("Response content:", response_app.text)
+# response_app = requests.get(str(pod_url)+":8069")
+# if response_app.status_code == 200:
+#     print("Request on app was successful")
+#     print("Your app is running....")
+# else:
+#     print("Request failed with status code:", response_app.status_code)
+#     print("Response content:", response_app.text)
