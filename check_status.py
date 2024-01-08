5a6d4616f8ebad011370cdbc5cb9c2661dc54199
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2024-01-08 13:37:34 +0500

    Update check_status.py

--- check_status.py
+++ check_status.py
@@ -4,7 +4,7 @@ import os
 update_url = os.environ['UPDATE_URL']
 auth_token = os.environ['AUTH_TOKEN']
 build_id = os.environ['BUILD_ID']
-pod_url = os.environ['POD_URL']
+# pod_url = os.environ['POD_URL']
 
 # params = {"id": str(db_id), "url": str(pod_url)+":8069"}
 params = {"id": str(build_id)}
