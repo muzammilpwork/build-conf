f5ca48bb1afe4bf31889c4130340f4394a439c49
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2024-01-09 13:25:39 +0500

    Update kuberntes_config.py

--- kuberntes_config.py
+++ kuberntes_config.py
@@ -33,6 +33,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
 
 RUN apt-get install -y python3-dev libxml2-dev libxslt1-dev zlib1g-dev libsasl2-dev libldap2-dev build-essential libssl-dev libffi-dev libmysqlclient-dev libjpeg-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev
 
+RUN apt install postgresql-client -y
 
 WORKDIR /opt/odoo
 
