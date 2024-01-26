4120d6748457b58ed22a85037463b1ff2dc148df
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2024-01-26 23:17:23 +0500

    Create clone_config.py

---
+++ clone_config.py
@@ -0,0 +1,21 @@
+import configparser
+import os
+
+
+serive_name = os.environ['DB_SERVICE_NAME']
+
+config = configparser.ConfigParser()
+
+
+config['options'] = {
+    'admin_passwd': 'abctest123',
+    'db_host': str(serive_name),
+    'db_port': '5432',
+    'db_user': 'odoo',
+    'db_password': 'odoo',
+    'addons_path': '/mnt/extra-addons/custom-addons',
+}
+
+
+with open("./config/odoo.conf", 'w') as configfile:
+    config.write(configfile)
