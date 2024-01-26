13ffc4b695903d1b505a4b6ab04156032efe59b3
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2024-01-26 23:44:52 +0500

    Update backup_restore.py

--- backup_restore.py
+++ backup_restore.py
@@ -5,7 +5,7 @@ import os
 existing_build_url = os.environ['EXISTING_BUILD_URL']
 new_build_url = os.environ['SUB_DOMAIN']
 
-url = f"https://nomanjallal-addons-new-test-app6yodttirbe3.erp-deploy.com/web/database/backup"
+url = f"{existing_build_url}/web/database/backup"
 data = {
     "master_pwd": 'abctest123',
     "name": 'postgres',
@@ -23,7 +23,7 @@ restore_payload = {
     'master_pwd': 'abctest123',
 }
 
-url = f'https://nomanjallal-addons-new-test-app7iz6ocf5gze.erp-deploy.com/web/database/restore'
+url = f'{new_build_url}.erp-deploy.com/web/database/restore'
 files = {'backup_file': file_content}
 
 restore_response = requests.post(url, data=restore_payload, files=files)
