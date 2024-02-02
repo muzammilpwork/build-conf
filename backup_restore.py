3f548cc82ab5647a5b0ba04627831f44d2b1e677
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2024-02-03 01:20:06 +0500

    Update backup_restore.py

--- backup_restore.py
+++ backup_restore.py
@@ -1,7 +1,6 @@
 import requests
 import os
 
-
 existing_build_url = os.environ['EXISTING_BUILD_URL']
 new_build_url = os.environ['SUB_DOMAIN']
 
@@ -27,4 +26,3 @@ url = f'https://{new_build_url}.erp-deploy.com/web/database/restore'
 files = {'backup_file': file_content}
 restore_response = requests.post(url, data=restore_payload, files=files)
 print("Data has been restored and response is: ", restore_response.content)
-
