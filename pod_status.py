f375dbd138db43cc2d69d3d7e3a2ef6aa10b82d7
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2024-02-07 00:35:52 +0500

    Create pod_status.py

---
+++ pod_status.py
@@ -0,0 +1,18 @@
+import subprocess
+import time
+pod_name = os.environ['APP_POD_NAME']
+
+command = 'kubectl get pod ' + str(pod_name) + ' -o jsonpath="{.status.phase}"'
+result = subprocess.run(command, shell=True, capture_output=True, text=True)
+pod_status = str(result.stdout).strip()
+
+count = 0
+while count < 3:
+    if pod_status == "Running":
+        break
+    else:
+      time.sleep(10)
+      count += 1
+
+if pod_status != "Running":
+  raise Exception("Pod is not in running state")
