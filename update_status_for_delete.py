80589cf1cca3eb7690400f0cfbb1742566aa6577
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2024-02-06 16:21:14 +0500

    Create update_status_for_delete.py

---
+++ update_status_for_delete.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+update_url = os.environ['UPDATE_URL']
+auth_token = os.environ['AUTH_TOKEN']
+build_id = os.environ['BUILD_ID']
+
+
+params = {"id": str(build_id)}
+headers = {"Authorization": f"Bearer {auth_token}"}
+
+response = requests.get(update_url, params=params, headers=headers)
+if response.status_code == 200:
+    print("Request was successful")
+    print("Response content:", response.text)
+else:
+    print("Request failed with status code:", response.status_code)
+    print("Response content:", response.text)
