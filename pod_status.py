0906c538fd2429dd3fe814c26dd977424f110ac6
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2024-02-07 02:22:22 +0500

    Update pod_status.py

--- pod_status.py
+++ pod_status.py
@@ -5,11 +5,11 @@ import os
 pod_name = os.environ['APP_POD_NAME']
 
 command = 'kubectl get pod ' + str(pod_name) + ' -o jsonpath="{.status.phase}"'
-result = subprocess.run(command, shell=True, capture_output=True, text=True)
-pod_status = str(result.stdout).strip()
 
 count = 0
 while count < 3:
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    pod_status = str(result.stdout).strip()
     if pod_status == "Running":
         break
     else:
