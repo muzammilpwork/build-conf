a160688caaef149446011aa4d04e5375f6c96f21
muzammilpwork <141617297+muzammilpwork@users.noreply.github.com>
2024-02-21 15:27:54 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 
 serive_name = os.environ['DB_SERVICE_NAME']
-db_name = os.environ['DB_NAME']
-without_demo = os.environ['WITHOUT_DEMO']
+# db_name = os.environ['DB_NAME']
+# without_demo = os.environ['WITHOUT_DEMO']
 
 # odoo_app_config_path = './debian/odoo.conf'
 # config = configparser.ConfigParser()
@@ -15,13 +15,15 @@ config = configparser.ConfigParser()
 
 
 config['options'] = {
-    'without_demo': str(without_demo),
+    # 'without_demo': str(without_demo),
+    'without_demo': 'True',
     'admin_passwd': 'abctest123',
     'db_host': str(serive_name),
     'db_port': '5432',
     'db_user': 'odoo',
     'db_password': 'odoo',
-    'db_name': str(db_name),
+    # 'db_name': str(db_name),
+    'db_name': 'noman123',
     'addons_path': '/mnt/extra-addons/custom-addons',
     'data_dir': '/var/lib/odoo',
     'list_db': 'True',
